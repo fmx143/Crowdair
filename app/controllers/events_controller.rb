3b4a204d9ded546f9a7b1975059085d965f7ad4e
medi gonul <medidem1997@hotmail.com>
2021-11-29 16:09:41 +0100

    Controllers show done

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -3,6 +3,8 @@ class EventsController < ApplicationController
   end
 
   def new
+    @event = Event.new
+    @user = current_user
   end
 
   def create
@@ -15,10 +17,12 @@ class EventsController < ApplicationController
   end
 
   def show
+    @event = Event.find(params[:id])
   end
 
   private
 
   def event_params
+    params.require(:event).permit(:title, :description, :end_date)
   end
 end
