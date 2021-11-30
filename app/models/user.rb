0012610c7a6aed01f9d400449bfc3917b8509547
Mallory Wittwer <mallory.wittwer@gmail.com>
2021-11-30 16:18:02 +0100

    Provide initial portfolio to signed up user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,24 @@
 class User < ApplicationRecord
+  after_save :add_initial_portfolio
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :buyer_transactions, class_name: 'Transaction', foreign_key: 'buyer_id'
   has_many :seller_transactions, class_name: 'Transaction', foreign_key: 'seller_id'
   has_many :investments
 
-  # validates :username, length: { in: 3..40 }
+  validates :username, length: { in: 3..60 }
 
   def transactions
     buyer_transactions + seller_transactions
+  end
+
+  def add_initial_portfolio
+    Event.all.each do |event|
+      Investment.create!({
+        user: self,
+        event: event,
+        n_actions: 10
+      })
+    end
   end
 end
