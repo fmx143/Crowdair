7becf66d970688e35b9e5b5738d3dd61283dd999
Mallory Wittwer <mallory.wittwer@gmail.com>
2021-12-02 09:24:56 +0100

    Add investment card prototype

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,7 @@ class UsersController < ApplicationController
     Event.joins(buyer_transactions: :current_user)
     @investments = Investment.all.where(user_id: current_user.id)
     @transactions = current_user.transactions.where.not(buyer_id: nil).order(updated_at: :desc).limit(10)
+    @offers = current_user.transactions.where(buyer_id: nil)
   end
 
   def update
