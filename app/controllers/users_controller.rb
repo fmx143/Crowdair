5f2da1d76aab0c1e06e04b2c304fce12b5c53422
Mallory Wittwer <mallory.wittwer@gmail.com>
2021-12-02 16:28:07 +0100

    Improves dashboard

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,18 @@ class UsersController < ApplicationController
     @investments = Investment.all.where(user_id: current_user.id)
     @transactions = current_user.transactions.where.not(buyer_id: nil).order(updated_at: :desc).limit(12)
     @offers = current_user.transactions.where(buyer_id: nil)
+    @total_participants = User.count
+    @ranking_position = User.order(points: :desc).pluck(:id).find_index(@user.id) + 1
+
+    # Compute data for portfolio graph in dashboard
+    total_trs = current_user.transactions.where.not(buyer_id: nil).order(updated_at: :desc)
+    balance = 0
+    @points_history = {}
+    total_trs.each do |transaction|
+      balance += transaction.n_actions * transaction.price
+      @points_history[transaction.updated_at] = balance
+    end
+
   end
 
   def update
