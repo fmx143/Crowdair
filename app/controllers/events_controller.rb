47a9e0eac24d06ca5a9cb35a86ff58d42d92c844
loic chapuis <loickoxxfox@hotmail.com>
2021-12-03 10:24:56 +0100

    visitors

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,13 +1,12 @@
 class EventsController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :index ]
+  skip_before_action :authenticate_user!, only: [:index]
   require 'uri'
   require 'net/http'
 
   def index
     @events = Event.all
-    @past_events = Event.where("end_date < ?", Time.now)    # Event.all.where(Date.today = Event.end_date)
+    # @past_events = Event.where("end_date < ?", Time.now)    # Event.all.where(Date.today = Event.end_date)
     # @events[1].end_date
-    raise
   end
 
   def new
