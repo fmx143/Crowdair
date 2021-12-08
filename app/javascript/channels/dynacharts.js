9060f7e23247fd46ec645c4c998acd7da64e2eb1
Mallory Wittwer <mallory.wittwer@gmail.com>
2021-12-08 14:59:30 +0100

    Fixes graphs display

--- app/javascript/channels/dynacharts.js
+++ app/javascript/channels/dynacharts.js
@@ -1,31 +1,41 @@
-const b1 = document.getElementById('set-day');
-const b2 = document.getElementById("set-hour");
-const lc1 = document.getElementById("chart-all");
-const lc2 = document.getElementById("chart-1h");
-
 const showChartAll = () => {
-  console.log("ran all");
+  const b1 = document.getElementById("set-day");
+  const b2 = document.getElementById("set-hour");
+  const lc1 = document.getElementById("chart-all");
+  const lc2 = document.getElementById("chart-1h");
   if (lc1) {
     lc1.classList.remove("hidden-chart");
+    b1.classList.add("highlighted");
     lc2.classList.add("hidden-chart");
-  }
-}
-
-const showChart1h = () => {
-  console.log("ran 1h");
-  if (lc1) {
-    lc1.classList.add("hidden-chart");
-    lc2.classList.remove("hidden-chart");
-  }
-}
-
-const initDynamicChart = () => {
-  showChartAll();
-  console.log("ran showChartAll");
-  if (b1) {
-    b1.addEventListener('click', () => { showChartAll() })
-    b2.addEventListener("click", () => { showChart1h() });
+    b2.classList.remove("highlighted");
   }
 };
 
-export { initDynamicChart }
+const showChart1h = () => {
+  const b1 = document.getElementById("set-day");
+  const b2 = document.getElementById("set-hour");
+  const lc1 = document.getElementById("chart-all");
+  const lc2 = document.getElementById("chart-1h");
+  if (lc1) {
+    lc1.classList.add("hidden-chart");
+    b1.classList.remove("highlighted");
+    lc2.classList.remove("hidden-chart");
+    b2.classList.add("highlighted");
+  }
+};
+
+const initDynamicChart = () => {
+  showChartAll();
+  const b1 = document.getElementById("set-day");
+  const b2 = document.getElementById("set-hour");
+  if (b1) {
+    b1.addEventListener("click", () => {
+      showChartAll();
+    });
+    b2.addEventListener("click", () => {
+      showChart1h();
+    });
+  }
+};
+
+export { initDynamicChart };
